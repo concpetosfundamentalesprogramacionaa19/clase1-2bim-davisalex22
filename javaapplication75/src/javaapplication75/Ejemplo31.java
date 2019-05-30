1348eadc999b7f3655f2277c259bd25058cd4a28
David Salazar <davis.dassr@gmail.com>
2019-05-30 11:27:39 -0500

    Clase sobre Arreglos

--- javaapplication75/src/javaapplication75/Ejemplo31.java
+++ javaapplication75/src/javaapplication75/Ejemplo31.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication75;
 
 import java.util.Scanner;
@@ -15,6 +10,7 @@ public class Ejemplo31 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
+        // Declaración de variables
         double nota = 0;
         int opcion = 0;
         boolean bandera = true;
@@ -22,17 +18,20 @@ public class Ejemplo31 {
         String[] rangos = {"0-8", "8.1-12", "12.1-16", "16.1-19", "19.1-20",
             "20.1-adelante"};
         double[] datos = new double[6];
+        // Uso de While para controlar el ingreso de notas conforme usuario
         while (bandera == true) {
             System.out.printf("Ingrese nota %d: ",
                     contador);
             nota = entrada.nextDouble();
             contador = contador + 1;
+            // Evaluacion de la continuidad de ingreso de notas
             System.out.println("Seleccione (1) Si desea seguir agregando o (2)"
                     + " si desea terminar\n");
             opcion = entrada.nextInt();
             if (opcion != 1) {
                 bandera = false;
             }
+            // Rangos a clasificar usando condicional If
             if (nota <= 8) {
                 datos[0] = datos[0] + 1;
             } else {
@@ -55,6 +54,7 @@ public class Ejemplo31 {
                 }
             }
         }
+        // Muestra en pantalla de resultados
         System.out.println("Resultados");
         for (int i = 0; i < datos.length; i++) {
             System.out.printf("Notas entre %s - %.0f\n", rangos[i], datos[i]);
